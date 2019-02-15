e568fc3dee8e19ce103e4dbe1dd373bcc3313038
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-02-15 07:06:32 -0800

    ft(update):create update - create update functionality - update route path
    [Finishes #163848719]

--- dummy/route/index.js
+++ dummy/route/index.js
@@ -4,11 +4,12 @@ import validation from '../validation/mealFieldValidation';
 
 const router = express.Router();
 
-const { getAllMeal, createMeal,deleteMeal  } = mealController;
+const { getAllMeal, createMeal,deleteMeal , updateMeal } = mealController;
 const { mealValidation } = validation;
 
 router.post('/meals', mealValidation, createMeal);
 router.get('/meals', getAllMeal);
-router.delete('meal/:id', deleteMeal)
+router.delete('/meal/:id', deleteMeal)
+router.put('/meal/:id', updateMeal)
 
 export default router;
